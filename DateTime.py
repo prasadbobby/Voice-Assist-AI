ffcde148366a03018cb1cd60514e2054ac31703d
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-19 10:33:59 +0530

    DateTime.py

--- DateTime.py
+++ DateTime.py
@@ -1,16 +1,16 @@
-from Package import *
-from Speak import *
-def time():
+from Package import *                                               # import packages from Package.py file in the same directory
+from Speak import *                                                 # import speak function from Speak.py file
+def time():                                                         # Created time() function.
   time = datetime.datetime.now().strftime("%I:%M:%S")
-  speak(time)
+  speak(time)                                                       # speak out the current time
   
-def date():
+def date():                                                         # created date() function.
   date = int(datetime.datetime.now().day)
   month = int(datetime.datetime.now().month)
   year = int(datetime.datetime.now().year)
-  speak(date)
-  speak(month)
-  speak(year)
+  speak(date)                                                       # speak out the current date
+  speak(month)                                                      # speak out the current month 
+  speak(year)                                                       # speak out the current year
   
   
   
