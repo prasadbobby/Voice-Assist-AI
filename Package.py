5572224cc408e7c0624b86867df3a82b1a05fa75
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-20 12:38:27 +0530

    Package.py

--- Package.py
+++ Package.py
@@ -1,3 +1,10 @@
-import pyttsx3 # used to convert text-to-speech in python
-import datetime # fetch and display the current date and time in python
-import wikipedia # fetch information from wikipedia site with the given user command
+import pyttsx3 
+import datetime 
+import wikipedia 
+import speech_recognition as sr
+import smtplib
+import webbrowser as wb
+import os
+import pyautogui                  
+import psutil                       
+import pyjokes
