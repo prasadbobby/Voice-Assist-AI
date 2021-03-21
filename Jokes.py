3687876ac951f19afe33f17eb0ca2e9f3ff25fad
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-21 12:30:32 +0530

    Jokes.py

--- Jokes.py
+++ Jokes.py
@@ -2,4 +2,4 @@ from Package import *
 from Speak import *
 
 def jokes():
-    speak(pyjokes.get_joke())
+    speak(pyjokes.get_joke())                       # pyjokes is a packages which gives a random joke from the internet.
