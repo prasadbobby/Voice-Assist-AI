aae82405bd8e04447d70fbc2001b42d20715e2c0
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-21 12:13:22 +0530

    Package.py

--- Package.py
+++ Package.py
@@ -1,10 +1,10 @@
-import pyttsx3 
+import pyttsx3                                          # pip install pyttsx3
 import datetime 
-import wikipedia 
-import speech_recognition as sr
-import smtplib
-import webbrowser as wb
-import os
-import pyautogui                  
+import wikipedia                                        # pip install wikipedia
+import speech_recognition as sr                         # pip install SpeechRecognition
+import smtplib  
+import webbrowser as wb                                 # pip install webbrowser
+import os 
+import pyautogui                                        # pip install pyautogui
 import psutil                       
 import pyjokes
