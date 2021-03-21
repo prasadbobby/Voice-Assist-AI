d9bf4a701865f117a147d642a1a467939814ccd7
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-21 12:16:21 +0530

    Package.py

--- Package.py
+++ Package.py
@@ -1,3 +1,7 @@
+# This is a package file which contains all the packages for this virtual assistant project.
+# You need to install this from your command prompt (terminal) incase you don't have any of this packages.
+# Before debugging this code make sure to check whether all the packages are installed correctly.
+
 import pyttsx3                                          # pip install pyttsx3
 import datetime 
 import wikipedia                                        # pip install wikipedia
